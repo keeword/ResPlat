d00008105edf22ecddb314b900a161212810b1b8
yuang-liao <yuang@example.com>
2015-01-23 23:34:17 +0800

    1.23 last

--- app/views/user/update.blade.php
+++ app/views/user/update.blade.php
@@ -42,37 +42,48 @@
 </div>
 </div>
 
-    <!-- Mainly scripts -->
+   <!-- Mainly scripts 
+     <script type="text/javascript" src="http://tajs.qq.com/stats?sId=9051096" charset="UTF-8"></script>
+     -->
     <script src="/js/jquery-1.10.2.js"></script>
-    <script src="/js/bootstrap.min.js?v=1.6"></script>
-    <script src="/js/plugins/metisMenu/jquery.metisMenu.js"></script>
     <script src="/js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-
-    <script src="/js/plugins/jeditable/jquery.jeditable.js"></script>
     <!-- jQuery Validation plugin javascript-->
-    <script src="js/plugins/validate/jquery.validate.min.js"></script>
-    <script src="js/plugins/validate/messages_zh.min.js"></script>
-    <!-- Data Tables -->
-    <script src="/js/plugins/dataTables/jquery.dataTables.js"></script>
-    <script src="/js/plugins/dataTables/dataTables.bootstrap.js"></script>
+    <script src="/js/plugins/validate/jquery.validate.min.js"></script>
+    <script src="/js/plugins/validate/messages_zh.min.js"></script>
+    
+    <!-- layer javascript -->
+    <script src="/js/plugins/layer/layer.min.js"></script>
+    
+    
     <script>
     //将form转为AJAX提交
     function ajaxSubmit(frm, fn) {
         $.ajax({
             url: frm.action,
             type: frm.method,
-            data: $('form#createuserform').serialize(),
+            data: $('form#updateuserform').serialize(),
+            dataType:'json',
             success: fn,
             beforeSend:function(){
-                if($('input[id=password]').val()!= $('input[id=repasswd]').val()){
-                    alert('unmatch!');
+                if(!(($('input[id=username]').val()).match(/^[a-zA-Z0-9_]{4,16}$/))){
+                    layer.load('请输入4-16位英文字符、数字或下划线的用户名',1);
+                    return false;
+                }else if($('input[id=nickname]').val().length==0){
+                    layer.load('部门名称不能为空',1);
+                    return false;
+                }else if(!$('input[id=password]').val().match(/^.{6,32}$/)){
+                    layer.load('请输入6-32位长度的密码',1);
+                    return false;
+                }else if($('input[id=repasswd]').val().length==0){
+                    layer.load('请确认密码',1);
+                    return false;
+                }else if($('input[id=password]').val()!= $('input[id=repasswd]').val()){
+                    layer.load('两次输入的密码不一致!', 1);
                     return false;
                 }
                 else if(($('input[id=username]').val().length&&$('input[id=password]').val().length)==0) {
-                    //alert('bunengweikong');
                     return false;
                 }else {
-                    //alert('buweikong');
                     return true;
                 }
             }
@@ -80,15 +91,17 @@
     }
 
     $(document).ready(function(){
-        $('#teuserform').bind('submit', function(){
+        $('#updateuserform').bind('submit', function(){
 
             ajaxSubmit(this, function(json){
 
                 if(json.success==true){
-                        layer.load('loading...', 3);
-                        window.location.href="{{ URL::route('home') }}";
+                        layer.load('帐号创建成功!!', 1);
+                        //window.location.href="{{ URL::route('home') }}";
                 }else{
-                    layer.load('帐号密码不匹配', 1);
+                        layer.load(json.error, 1);
+
+                    //layer.load('帐号密码不匹配', 1);
                 }
             });
             return false;
